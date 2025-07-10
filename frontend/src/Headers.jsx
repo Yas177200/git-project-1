69ef90d2f3b22cedcf8e4f3723b124a0d2c8d220
Yasser Abdulmala <90008483+Yas177200@users.noreply.github.com>
2025-07-10 19:12:30 +0200

    first commit

---
+++ frontend/src/Headers.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Headers() {
+  return (
+    <>
+     <div><h1>Welcome to Group A react platform</h1></div>
+        <div><h1 style={{color:"green"}}>Teacher: Ahmed Shaashaa</h1></div>
+        <div><h2 style={{color:"red"}}>Student: Yasser Abdulmala</h2></div>
+        <div><h2 style={{color:"red"}}>Student: Reem Asaad</h2></div>
+        <div><h2 style={{color:"red"}}>Student: Reem Abdullah</h2></div>
+        <div><h2 style={{color:"red"}}>Student: Mahdi AL Hariri</h2></div>
+        <div><h2 style={{color:"red"}}>Student: Amina Alfadawiee</h2></div>
+        <div><h2 style={{color:"red"}}>Student: haitham almohammad</h2></div>
+        <div><h2 style={{color:"red"}}>Student: mohammad alfares</h2></div>
+    </>
+
+  )
+}
+
+export default Headers
